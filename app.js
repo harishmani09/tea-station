e545e2b285a5ea2e516abda265e408931e320a05
harishmani09 <harishmani@gmail.com>
2021-08-08 12:43:00 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-//setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
 // setup nav
+// const navBtn = document.getElementById("nav-btn");
+// const navbar = document.getElementById("navbar");
+// const navClose = document.getElementById("nav-close");
+// show nav
+// navBtn.addEventListener("click", () => {
+//   navbar.classList.add("showNav");
+// });
+// close nav
+// navClose.addEventListener("click", () => {
+//   navbar.classList.remove("showNav");
+// });
+//setup date
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
+
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
-// show nav
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
-});
-// close nav
+
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
+});
+
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
 });
